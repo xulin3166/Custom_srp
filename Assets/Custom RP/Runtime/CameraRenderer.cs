20f018c820517b61d330af115283c85f912c3717
xulin3166 <59166443+xulin3166@users.noreply.github.com>
2020-12-07 20:58:34 +0800

    工程创建
    + add

---
+++ Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer
+{
+    ScriptableRenderContext context;
+    Camera camera;
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        Setup();
+        DrawVisibleGeometry();
+        Submit();
+    }
+
+    void DrawVisibleGeometry() 
+    {
+        context.DrawSkybox(camera);
+    }
+
+    void Submit()
+    {
+        context.Submit();
+    }
+
+    void Setup() {
+        context.SetupCameraProperties(camera);
+    }
+}
