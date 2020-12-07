d7f74b79fc8c6055797d29fddc9ade7b7ecfac47
xulin3166 <xulin3166@qq.com>
2020-12-07 23:56:03 +0800

    update srp

--- Assets/Custom RP/Runtime/CameraRenderer.cs
+++ Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -8,10 +8,19 @@ public class CameraRenderer
     ScriptableRenderContext context;
     Camera camera;
 
+    const string bufferName = "Camera Renderer";
+    CommandBuffer buffer = new CommandBuffer() { name = bufferName };
+    CullingResults cullingResults;
+
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
     public void Render(ScriptableRenderContext context, Camera camera)
     {
         this.context = context;
         this.camera = camera;
+
+        if (!Cull())
+            return;
 
         Setup();
         DrawVisibleGeometry();
@@ -20,15 +29,41 @@ public class CameraRenderer
 
     void DrawVisibleGeometry() 
     {
+        var sortingSettings = new SortingSettings(camera);
+        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+        var filteringSettings = new FilteringSettings(RenderQueueRange.all);
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
         context.DrawSkybox(camera);
     }
 
     void Submit()
     {
+        buffer.EndSample(bufferName);
+        ExecuteBuffer();
         context.Submit();
     }
 
     void Setup() {
         context.SetupCameraProperties(camera);
+
+        buffer.ClearRenderTarget(true, true, Color.clear);
+        buffer.BeginSample(bufferName);   
+        ExecuteBuffer();
+    }
+
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+    bool Cull()
+    {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
     }
 }
